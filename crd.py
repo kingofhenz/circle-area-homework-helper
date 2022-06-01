ecefd84dd20ea50d07267956733b81bcf9ac6b86
kingofhenz <93614808+kingofhenz@users.noreply.github.com>
2022-06-01 18:17:04 -0400

    Update crd.py

--- crd.py
+++ crd.py
@@ -13,3 +13,22 @@ while True:
         circumference = 2 * pi * radius
         print("The circumference of the circle is: " + str(circumference))
         print("******************************************************************************************")
+    if crd == "diameter":
+        diameter = float(radius)
+        area = pi * radius * radius
+        print("The area of the circle is: " + str(area))
+        radius = diameter / 2
+        circumference = 2 * pi * radius
+        print("The circumference of the circle is: " + str(circumference))
+        print("******************************************************************************************")
+    if crd == "circumference":
+        circumference = float(radius)
+        area = pi * radius * radius
+        print("The area of the circle is: " + str(area))
+        radius = circumference / (2 * pi)
+        diameter = 2 * radius
+        print("The diameter of the circle is: " + str(diameter))
+        print("******************************************************************************************")
+    if crd != "radius" or "diameter" or "circumference":
+            print("error")
+            print("******************************************************************************************")
