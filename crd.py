808e75f74a508cb73d4a71cf6ccbbfa2d1889785
kingofhenz <93614808+kingofhenz@users.noreply.github.com>
2022-06-08 15:56:29 -0400

    Update crd.py

--- crd.py
+++ crd.py
@@ -3,7 +3,7 @@ print("*************************************************************************
 while True:
     pi = 3.141592653589793238462643383279
     crd = input("do you have the circumfrence, radius, or diameter: ")
-    irf = (input("Enter the " + crd  + " " + "of the circle: "))
+    irf = float(input("Enter the " + crd  + " " + "of the circle: "))
     if crd == "radius":
         radius = float(radius)
         area = pi * radius * radius
@@ -14,18 +14,19 @@ while True:
         print("The circumference of the circle is: " + str(circumference))
         print("******************************************************************************************")
     if crd == "diameter":
+        irf = float(irf)
         area = (irf / 2 * 2 ) * pi
         print("The area of the circle is: " + str(area))
         radius = diameter / 2
         circumference = 2 * pi * radius
-        print("The circumference of the circle is: " + str(circumference))
+        print("The circumference of the circle is: " + circumference)
         print("******************************************************************************************")
     if crd == "circumference":
         area = irf / pi / 2 * pi 
-        print("The area of the circle is: " + str(area))
+        print("The area of the circle is: " + area)
         radius = circumference / (2 * pi)
         diameter = 2 * radius
-        print("The diameter of the circle is: " + str(diameter))
+        print("The diameter of the circle is: " + diameter)
         print("******************************************************************************************")
     if crd != "radius" or "diameter" or "circumference":
             print("error")
