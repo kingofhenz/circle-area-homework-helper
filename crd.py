f1b5f8a9d4bc461c9fc530dfb7e909397f8d2d46
kingofhenz <93614808+kingofhenz@users.noreply.github.com>
2022-06-08 16:00:35 -0400

    Update crd.py

--- crd.py
+++ crd.py
@@ -1,7 +1,7 @@
 print("A program for calculate the Area,Diameter and Circumference of a circle if Radius, diameter, or circumference is known")
 print("******************************************************************************************")
 while True:
-    pi = 3.141592653589793238462643383279
+    pi = 3.14
     crd = input("do you have the circumfrence, radius, or diameter: ")
     irf = float(input("Enter the " + crd  + " " + "of the circle: "))
     if crd == "radius":
@@ -17,9 +17,9 @@ while True:
         irf = float(irf)
         area = (irf / 2 * 2 ) * pi
         print("The area of the circle is: " + str(area))
-        radius = diameter / 2
+        radius = irf / 2
         circumference = 2 * pi * radius
-        print("The circumference of the circle is: " + circumference)
+        print("The circumference of the circle is: " + str(circumference))
         print("******************************************************************************************")
     if crd == "circumference":
         area = irf / pi / 2 * pi 
@@ -28,6 +28,3 @@ while True:
         diameter = 2 * radius
         print("The diameter of the circle is: " + diameter)
         print("******************************************************************************************")
-    if crd != "radius" or "diameter" or "circumference":
-            print("error")
-            print("**************************************************************************************")
